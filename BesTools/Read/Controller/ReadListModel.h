cbec4d7302c38e288b6b929c114811ca90bed61d
1129530686 <1129530686@qq.com>
2016-08-24 13:34:12 +0800

    afnetworking

---
+++ BesTools/Read/Controller/ReadListModel.h
@@ -0,0 +1,17 @@
+//
+//  ReadListModel.h
+//  BesTools
+//
+//  Created by pan on 16/8/24.
+//  Copyright © 2016年 Aaron Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReadListModel : NSObject
+@property (assign,nonatomic) NSString *ctime;
+@property (assign,nonatomic) NSString *Description;
+@property (assign,nonatomic) NSString *picUrl;
+@property (assign,nonatomic) NSString *title;
+@property (assign,nonatomic) NSString *url;
+@end
---
+++ BesTools/Read/Controller/ReadTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ReadTableViewCell.m
+//  BesTools
+//
+//  Created by pan on 16/8/24.
+//  Copyright © 2016年 Aaron Lee. All rights reserved.
+//
+
+#import "ReadTableViewCell.h"
+
+@implementation ReadTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
