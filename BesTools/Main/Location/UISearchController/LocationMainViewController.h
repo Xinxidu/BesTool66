215d41dda9200351ebe57ae6fe100c825c6167f3
1129530686 <1129530686@qq.com>
2016-08-26 16:28:50 +0800

    地图定位搜索

---
+++ BesTools/Main/Location/UISearchController/LocationMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationMainViewController.h
+//  BesTools
+//
+//  Created by pan on 16/8/26.
+//  Copyright © 2016年 Aaron Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationMainViewController : UIViewController
+
+@end
